9825665ad4c3faee6976f04a74836d1cfef2a09a
Francisco <francisco.santossouzafilho@gmail.com>
2023-03-04 17:40:17 -0400

    API implemntado o jwt

--- src/routes/v1/pessoa.ts
+++ src/routes/v1/pessoa.ts
@@ -1,15 +1,16 @@
 import { create, destroy, show, update } from 'controllers/pessoa';
 import { list } from 'controllers/pessoa';
 import { Router } from 'express';
+import { checkJwt } from 'middleware/checkJwt';
 
 
 const router = Router();
 
-router.get('/',list);
-router.post('/',create)
-router.get('/:id',show)
-router.put('/:id',update)
-router.delete('/:id',destroy)
+router.get('/',[checkJwt],list);
+router.post('/',[checkJwt],create)
+router.get('/:id',[checkJwt],show)
+router.put('/:id',[checkJwt],update)
+router.delete('/:id',[checkJwt],destroy)
 
 
 export default router;
