e6172e70ea1bfcd60cd66b5bbf578f2f3d9576df
Francisco <francisco.santossouzafilho@gmail.com>
2023-03-05 18:38:21 -0400

    Pesquisa por ID

--- src/controllers/users/show.ts
+++ src/controllers/users/show.ts
@@ -17,7 +17,8 @@ export const show = async (req: Request, res: Response, next: NextFunction) => {
       const customError = new CustomError(404, 'General', `User with id:${id} not found.`, ['User not found.']);
       return next(customError);
     }
-    res.customSuccess(200, 'User found', user);
+    res.json(user)
+    //res.customSuccess(200, 'User found', user);
   } catch (err) {
     const customError = new CustomError(400, 'Raw', 'Error', null, err);
     return next(customError);
